5a4e21727f801277f5ddd667ba6b2168a71a630b
Clemens Sahs <csahs@slides-worker.org>
2011-07-20 22:03:37 +0200

    delete demos

--- src/demo/Slides/Http/Query/__global.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-include dirname(__FILE__) . DIRECTORY_SEPARATOR .
-                            '..' . DIRECTORY_SEPARATOR . '__global.php';
--- src/demo/Slides/__global.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( 'DIR_SEP' , DIRECTORY_SEPARATOR );
-define( 'DIR_BACK' , '..' . DIR_SEP );
-define( 'DIR_SLIDES_DEMO_BASE' , realpath(dirname(__FILE__)) . DIR_SEP );
-define( 'DIR_SLIDES_LIB' , realpath(dirname(__FILE__) . DIR_SEP . DIR_BACK . DIR_BACK ) . DIR_SEP . 'library' . DIR_SEP);
-
-include_once DIR_SLIDES_DEMO_BASE . '__autoloader.php';
